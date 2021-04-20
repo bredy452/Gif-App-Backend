d66c5ac8237cf1896c34232b964192cf6802fd39
bredy452 <75282516+bredy452@users.noreply.github.com>
2021-04-19 21:32:49 -0400

    Delete models directory

--- models/gifs.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const {Schema, model} = mongoose;
-
-const gifSchema = new Schema({
-   gifId: String, 
-   numberOfLikes: Number
-});
-
-module.exports = model('Gif', gifSchema)
